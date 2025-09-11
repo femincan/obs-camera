dadff648fb90720d5cea126d0e815a4c8568bb2f
Furkan Emin Can <femincanx@gmail.com>
2025-09-11 18:34:41 +0300

    feat: add base server setup with local ip middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { websocket } from 'hono/bun';
+import { getLocalIP } from './lib/utils.js';
 
-const app = new Hono()
+const app = new Hono<{ Variables: { localIP: string } }>();
+
+app.use('*', async (c, next) => {
+  const localIP = getLocalIP();
+
+  if (localIP === null) {
+    c.status(500);
+    return c.json({ error: 'Server IP is not found!' });
+  }
+
+  c.set('localIP', localIP);
+  await next();
+});
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-export default app
+const CERT_DIR_PATH = `${import.meta.dir}/../cert`;
+const server = Bun.serve({
+  fetch: app.fetch,
+  port: 3000,
+  hostname: '0.0.0.0',
+  tls: {
+    cert: Bun.file(`${CERT_DIR_PATH}/cert.pem`),
+    key: Bun.file(`${CERT_DIR_PATH}/key.pem`),
+  },
+  websocket,
+});
+console.log(`The server is running at: https://${getLocalIP()}:${server.port}`);
