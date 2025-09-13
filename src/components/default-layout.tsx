9fe7a1e201a26802b527c7ff58d6cc3651833a3f
Furkan Emin Can <femincanx@gmail.com>
2025-09-13 19:11:08 +0300

    feat: add base styles

--- src/components/default-layout.tsx
+++ src/components/default-layout.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from 'hono/jsx';
+import { css, Style } from 'hono/css';
 
 export function DefaultLayout({ children }: PropsWithChildren) {
   return (
@@ -7,6 +8,22 @@ export function DefaultLayout({ children }: PropsWithChildren) {
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>OBS Camera</title>
+        <Style>
+          {css`
+            * {
+              margin: 0;
+              padding: 0;
+            }
+            html,
+            body {
+              height: 100vh;
+              width: 100vw;
+            }
+            body {
+              background-color: black;
+            }
+          `}
+        </Style>
       </head>
       <body>{children}</body>
     </html>
