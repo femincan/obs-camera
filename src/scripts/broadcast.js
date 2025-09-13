1f3dbc9033dc7e30309367913f0ee07e9d09e372
Furkan Emin Can <femincanx@gmail.com>
2025-09-13 18:07:46 +0300

    feat: add content of broadcast.js file

--- src/scripts/broadcast.js
+++ src/scripts/broadcast.js
@@ -1,1 +1,56 @@
-console.log('Hello World');
+const pc = new RTCPeerConnection();
+
+ws.addEventListener('open', async () => {
+  const stream = await getUserMedia();
+
+  if (!stream) return;
+
+  ws.send(JSON.stringify({ type: 'broadcaster' }));
+
+  stream.getTracks().forEach((track) => pc.addTrack(track, stream));
+
+  pc.addEventListener('icecandidate', (e) => {
+    if (e.candidate) {
+      ws.send(JSON.stringify({ type: 'candidate', candidate: e.candidate }));
+    }
+  });
+
+  ws.addEventListener('message', async (e) => {
+    const data = JSON.parse(e.data);
+
+    switch (data.type) {
+      case 'offer':
+        {
+          const offer = await pc.createOffer();
+          await pc.setLocalDescription(offer);
+          ws.send(JSON.stringify(offer));
+        }
+        break;
+      case 'answer':
+        await pc.setRemoteDescription(data);
+        break;
+      case 'candidate':
+        await pc.addIceCandidate(data.candidate);
+        break;
+    }
+  });
+});
+
+async function getUserMedia() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'environment',
+        aspectRatio: 1,
+        frameRate: 60,
+        width: 1080,
+        height: 1080,
+        maxWidth: 1080,
+        maxHeight: 1080,
+      },
+    });
+    return stream;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
