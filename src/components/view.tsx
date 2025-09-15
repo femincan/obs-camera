70d1283a4d3b9e7faec27dd12f50247c5f359b41
Furkan Emin Can <femincanx@gmail.com>
2025-09-15 09:31:27 +0300

    feat: add styles for view route

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -1,10 +1,30 @@
+import { css } from 'hono/css';
 import { generateScript } from '../lib/utils';
 import viewScript from '../scripts/view' with { type: 'text' };
+
+const containerClass = css`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > video {
+    display: block;
+    width: 1080px;
+    max-width: 100%;
+    max-height: 100%;
+    aspect-ratio: 1 / 1;
+    object-fit: cover;
+  }
+`;
 
 export function View({ ip, port }: { ip: string; port: number }) {
   return (
     <>
-      <video autoplay playsinline muted></video>
+      <div class={containerClass}>
+        <video autoplay playsinline muted></video>
+      </div>
       <script
         dangerouslySetInnerHTML={{
           __html: generateScript([viewScript], ip, port),
